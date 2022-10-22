4274040837990eea232633b0fa5f3b448e96e710
germanUNAP <ccus.german.chura@gmail.com>
2022-10-23 05:37:04 +1300

    se crea la carpeta para el analisis

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,4 @@
-//@ts-check
-
-(()=> {
+(async ()=> {
   const myCart = [];
   const products = [];
   const limit = 2;
@@ -9,25 +7,25 @@
     const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
       mehtod: 'GET'
     });
-    const data = await rta.parseJson();
+    const data = await rta.json();
     products.concat(data);
   }
   function getTotal() {
-    const total = 0;
-    for (const i = 0; i < products.length(); i++) {
-      total += products[i].prize;
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+      total += products[i];
     }
     return total;
   }
   function addProduct(index) {
-    if (getTotal <= limit) {
+    if (getTotal() <= limit) {
       myCart.push(products[index]);
     }
   }
 
   await getProducts();
-  addProducto(1);
-  addProducto(2);
+  addProduct(1);
+  addProduct(2);
   const total = getTotal();
   console.log(total);
   const person = {
